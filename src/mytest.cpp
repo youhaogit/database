96bf9bd864fc23fc7cf87c9ea5a3df330d012e10
youhaogit <youhaouci@gmail.com>
2018-11-07 11:41:56 -0800

    first commit

---
+++ src/mytest.cpp
@@ -0,0 +1,19 @@
+/*
+ * mytest.cpp
+ *
+ *  Created on: Nov 5, 2018
+ *      Author: youhao
+ */
+
+#include <iostream>
+
+int main() {
+int sum = 0;
+  for (int k=1; k<11; k++)   {
+    sum += k;
+  }
+  std::cout << "The sum is: " << sum << std::endl;
+  return 0;
+}
+
+
